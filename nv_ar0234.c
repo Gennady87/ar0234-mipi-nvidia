3f096753d6e852878a92583d8b26461a1ec9063e
DaniusKalv <danius@kurokesu.com>
2026-01-20 10:54:07 +0200

    Optimize multiregister i2c reads/writes

--- nv_ar0234.c
+++ nv_ar0234.c
@@ -64,8 +64,6 @@ static const struct regmap_config sensor_regmap_config = {
 	.reg_bits = 16,
 	.val_bits = 8,
 	.cache_type = REGCACHE_RBTREE,
-	.use_single_read = true,
-	.use_single_write = true,
 };
 
 static inline int ar0234_read_reg(struct camera_common_data *s_data, u16 addr,
