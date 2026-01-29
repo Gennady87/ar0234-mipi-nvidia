ed9b7c02626fed9eda8524b4770e272b54de74f8
DaniusKalv <danius@kurokesu.com>
2026-01-29 08:35:48 +0200

    Fix setup script module path

--- setup.sh
+++ setup.sh
@@ -10,8 +10,7 @@ echo "Copying prebuilt device tree entry to /boot"
 cp $DRV_FILES_DIR/tegra234-p3767-camera-p3768-ar0234-A.dtbo /boot
 
 echo "Installing kernel module"
-mkdir -p /lib/modules/$(uname -r)/extra
-cp $DRV_FILES_DIR/nv_ar0234.ko /lib/modules/$(uname -r)/extra
+cp $DRV_FILES_DIR/nv_ar0234.ko /lib/modules/$(uname -r)/updates/drivers/media/i2c
 depmod
 echo nv_ar0234 | tee /etc/modules-load.d/kurokesu.conf
 
